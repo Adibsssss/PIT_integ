ca1c31533d1979dce30a801f40864636e1f65487
Isabella Gonzales <gonzales.isabella1414@gmail.com>
2026-03-13 22:37:17 +0800

    feat: add product detail, update, and delete endpoints

--- backend/adetxinteg/views.py
+++ backend/adetxinteg/views.py
@@ -55,3 +55,28 @@ def product_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    if not request.user.is_staff:
+        return Response({'detail': 'Not authorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
